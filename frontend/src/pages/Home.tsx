6315c4bb3512695b5047974372336a5f00d10dfe
Arpan Mondal <139039003+arpan-mondal-3000@users.noreply.github.com>
2025-02-07 22:36:04 +0530

    Google Oauth login setup

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,10 +1,33 @@
 import { Link } from "react-router";
 import Button from "../components/Button";
-import logo from "../assets/google.svg";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { GoogleLogin, CredentialResponse } from "@react-oauth/google";
 
 function Home() {
-  const [isLogged, setIsLogged] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [message, setMessage] = useState("");
+
+  const handleLoginSuccess = (response: CredentialResponse) => {
+    console.log("Google login successful: ", response);
+    if (response.credential) localStorage.setItem("token", response.credential);
+    setIsLoggedIn(true);
+    setMessage("Successfully logged in 🚀");
+  };
+
+  const handleGoogleLoginFailure = () => {
+    console.log("Google Login Failed!");
+    setIsLoggedIn(false);
+    setMessage("Google Login failed! Please try again.");
+  };
+
+  useEffect(() => {
+    const credentials = localStorage.getItem("token");
+    if (credentials) {
+      setIsLoggedIn(true);
+      // console.log(credentials);
+    }
+  }, []);
+
   return (
     <>
       <nav className="absolute z-10 w-full font-primary text-[2rem] font-black justify-between items-center top-0  p-4 border-b-2 border-gray-300">
@@ -14,7 +37,6 @@ function Home() {
           </div>
         </div>
       </nav>
-
 
       <div>
         <div className="flex flex-col h-screen items-center justify-center px-4">
@@ -35,22 +57,35 @@ function Home() {
               <Button title="Scan Prescription" />
             </Link>
           </div>
-          {!isLogged && (
+          {!isLoggedIn && (
             <div className="flex flex-col items-center justify-center bg-white mt-16 p-6 rounded-2xl shadow-lg w-full max-w-sm">
               {/* Login Text */}
               <p className="text-center font-semibold text-lg text-gray-800">
-                To save your scan, please login with Google
+                To save your scan, please sign in with Google
               </p>
 
               {/* Google Login Button */}
-              <button className="flex items-center justify-center gap-3 bg-gray-100 hover:bg-gray-200 transition-all text-gray-700 font-semibold px-5 py-3 rounded-lg shadow-md mt-4 w-full">
-                <img src={logo} alt="Google" className="w-6 h-6" />
-                Login With Google
-              </button>
+              <div className="my-4">
+                <GoogleLogin
+                  onSuccess={handleLoginSuccess}
+                  onError={handleGoogleLoginFailure}
+                />
+              </div>
             </div>
           )}
-
-
+          {message !== "" && (
+            <div className="flex flex-col items-center justify-center bg-white mt-16 p-6 rounded-2xl shadow-lg w-full max-w-sm">
+              <p
+                className={`text-center font-semibold text-lg ${
+                  message.includes("Successfully")
+                    ? "text-green-500"
+                    : "text-red-500"
+                }`}
+              >
+                {message}
+              </p>
+            </div>
+          )}
         </div>
       </div>
     </>
